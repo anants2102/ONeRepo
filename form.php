4338f02b5ce5370653cacd63b6332da6b4fb740a
anants2102 <anants231002@gmail.com>
2024-05-14 21:12:39 +0530

    rmvcom

--- form.php
+++ form.php
@@ -1,35 +1,18 @@
 <?php
-    if(isset($_POST['submit']))
-    {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
-    }
-
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "sampledb";
-
-    $con = mysqli_connect($host, $username, $password, $dbname);
-
-    // to ensure that the connection is made
+    $con = mysqli_connect('localhost', 'root', "", 'db_con');
     if (!$con)
     {
         die("Connection failed!" . mysqli_connect_error());
     }
-
-    // using sql to create a data entry query
-    $sql = "INSERT INTO contactform_entries (id, name, email, subject,message) VALUES ('0', '$name', '$email' ,'$subject','$message' )";
-  
-    // send query to the database to add values and confirm if successful
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $sql = "INSERT INTO contact_table (id, Name_field, Email_field, Subject_field, Message_field) VALUES ('0', '$name', '$email' ,'$subject','$message' )";
     $rs = mysqli_query($con, $sql);
     if($rs)
     {
         echo "Entries added!";
     }
-  
-    // close connection
     mysqli_close($con);
 ?>
